b15283790bd3beac6b337bbe32b118738db8e99e
Sebastian Gebski <sebastian@gebski.net>
2018-04-14 23:27:32 +0200

    Starting with layout -> SemanticUI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Container, Divider, Grid, Header, Segment } from 'semantic-ui-react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +7,44 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+        <Grid columns={16}>
+            <Grid.Row>
+              <Grid.Column width={16}>top</Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column floated='left' width={4}>
+                <Segment.Group>
+                  <Segment>
+                    <p>Channels</p>
+                  </Segment>
+                  <Segment.Group>
+                    <Segment>
+                      <p>Channel #1</p>
+                    </Segment>
+                    <Segment>
+                      <p>Channel #2</p>
+                    </Segment>
+                    <Segment>
+                      <p>Channel #3</p>
+                    </Segment>
+                  </Segment.Group>
+                </Segment.Group>
+              </Grid.Column>
+              <Grid.Column width={12}>
+                <Container>
+                  <Header as='h4' attached='top' block>Channel name</Header>
+                  <Segment attached>Segment</Segment>
+
+                  <Divider section />
+                </Container>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column width={16}>bottom</Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
       </div>
     );
   }
